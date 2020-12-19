564226e85de95c09141ec4d56ac2962482c7aff0
Keshava Munegowda <keshava.munegowda@gmail.com>
2020-12-19 14:54:16 +0530

    file system benchmarking using SBK APIs.
    Signed-off-by: Keshava Munegowda <keshava.munegowda@gmail.com>

---
+++ sbk-file/src/main/java/File.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) KMG. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsFactory;
+import io.sbk.api.DataType;
+import io.sbk.api.Storage;
+import io.sbk.api.Parameters;
+import io.sbk.api.Writer;
+import io.sbk.api.Reader;
+import io.sbk.api.impl.NioByteBuffer;
+import io.sbk.api.impl.Sbk;
+import org.apache.commons.cli.ParseException;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Class for File System Benchmarking using File Channel.
+ */
+public class File implements Storage<ByteBuffer> {
+    private final static String CONFIGFILE = "file.properties";
+    private FileConfig config;
+    private DataType<ByteBuffer> dType;
+
+    @Override
+    public void addArgs(final Parameters params) throws IllegalArgumentException {
+        final ObjectMapper mapper = new ObjectMapper(new JavaPropsFactory())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            config = mapper.readValue(
+                    Objects.requireNonNull(File.class.getClassLoader().getResourceAsStream(CONFIGFILE)),
+                    FileConfig.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new IllegalArgumentException(ex);
+        }
+        params.addOption("file", true, "File name, default file name : "+config.fileName);
+    }
+
+    @Override
+    public void parseArgs(final Parameters params) throws IllegalArgumentException {
+        config.fileName =  params.getOptionValue("file", config.fileName);
+        if (params.getWritersCount() > 1) {
+            throw new IllegalArgumentException("Writers should be only 1 for File writing");
+        }
+        if (params.getReadersCount() > 0 && params.getWritersCount() > 0) {
+            throw new IllegalArgumentException("Specify either Writer or readers ; both are not allowed");
+        }
+    }
+
+    @Override
+    public void openStorage(final Parameters params) throws  IOException {
+        if (config.reCreate && params.getWritersCount() > 0) {
+            java.io.File file = new java.io.File(config.fileName);
+            file.delete();
+        }
+    }
+
+    @Override
+    public void closeStorage(final Parameters params) throws IOException {
+
+    }
+
+    @Override
+    public Writer<ByteBuffer> createWriter(final int id, final Parameters params) {
+        try {
+            return new FileWriter(id, params, config);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Reader<ByteBuffer> createReader(final int id, final Parameters params) {
+        try {
+            return new FileReader(id, params, dType, config);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public DataType<ByteBuffer> getDataType() {
+        dType = new NioByteBuffer();
+        return dType;
+    }
+
+    public static void main(final String[] args) {
+        Storage device = new File();
+        try {
+            Sbk.run(args, device, "sbk-file");
+        } catch (ParseException | IllegalArgumentException | IOException |
+                InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
+
+
+
---
+++ sbk-file/src/main/java/FileWriter.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) KMG. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+import io.sbk.api.DataType;
+import io.sbk.api.Parameters;
+import io.sbk.api.SendChannel;
+import io.sbk.api.Status;
+import io.sbk.api.Time;
+import io.sbk.api.Writer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+ * Class for File Channel Writer.
+ */
+public class FileWriter implements Writer<ByteBuffer> {
+    final private FileChannel out;
+    final private FileConfig config;
+
+    public FileWriter(int id, Parameters params, FileConfig config) throws IOException {
+        this.config = config;
+        if (config.isAppend) {
+            this.out = FileChannel.open(Paths.get(config.fileName), StandardOpenOption.WRITE,
+                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } else {
+            this.out = FileChannel.open(Paths.get(config.fileName), StandardOpenOption.WRITE,
+                    StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+    }
+
+    @Override
+    public void recordWrite(DataType<ByteBuffer> dType, ByteBuffer data, int size, Time time,
+                            Status status, SendChannel record, int id) throws IOException {
+        final ByteBuffer buffer = data.asReadOnlyBuffer();
+        status.startTime = time.getCurrentTime();
+        out.write(buffer);
+        status.endTime = time.getCurrentTime();
+        status.bytes = size;
+        status.records = 1;
+        record.send(id, status.startTime, status.endTime, size, 1);
+    }
+
+
+    @Override
+    public CompletableFuture writeAsync(ByteBuffer data) throws IOException {
+        final ByteBuffer buffer = data.asReadOnlyBuffer();
+        out.write(buffer);
+        return null;
+    }
+
+    @Override
+    public void sync() throws IOException {
+        out.force(config.metaUpdate);
+    }
+
+    @Override
+    public void close() throws  IOException {
+        out.close();
+    }
+}
