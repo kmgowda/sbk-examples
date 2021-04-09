8e8c9a5f22abc0c800457f06b4b604529577b6e0
Keshava Munegowda <keshava.gowda@gmail.com>
2021-04-09 21:12:35 +0530

    Update to SBK API Release 0.87
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-file/src/main/java/FileWriter.java
+++ sbk-file/src/main/java/FileWriter.java
@@ -10,10 +10,10 @@
 
 import io.sbk.api.DataType;
 import io.sbk.api.Parameters;
-import io.sbk.api.SendChannel;
-import io.sbk.api.Status;
-import io.sbk.api.Time;
+import io.sbk.perl.SendChannel;
+import io.sbk.perl.Time;
 import io.sbk.api.Writer;
+import io.sbk.api.Status;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
