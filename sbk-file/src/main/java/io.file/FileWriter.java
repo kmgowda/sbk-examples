6962a8e0469778fef9eee1b3a26ffdb7a9370f6a
Keshava Munegowda <keshava.gowda@gmail.com>
2022-03-01 11:21:31 +0530

    Update to latest version 0.97
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-file/src/main/java/io.file/FileWriter.java
+++ sbk-file/src/main/java/io.file/FileWriter.java
@@ -10,7 +10,7 @@
 package io.file;
 import io.sbk.data.DataType;
 import io.sbk.api.ParameterOptions;
-import io.perl.PerlChannel;
+import io.perl.api.PerlChannel;
 import io.sbk.api.Status;
 import io.time.Time;
 import io.sbk.api.Writer;
